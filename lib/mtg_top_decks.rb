f1acd17c26720a9c2fb019f96d2bf994d9251183
Matthew Jones <matts3371@gmail.com>
2020-01-13 13:43:58 -0600

    A working interface with fake data

---
+++ lib/mtg_top_decks.rb
@@ -0,0 +1,6 @@
+require "mtg_top_decks/version"
+
+module MtgTopDecks
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mtg_top_decks/version.rb
@@ -0,0 +1,3 @@
+module MtgTopDecks
+  VERSION = "0.1.0"
+end
